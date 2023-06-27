e21a4193c7fb735982d4dff8fef073fb190b21b0
Smilla Dion <smilla.dion@live.dk>
2023-06-27 15:26:18 +0200

    Add arrow icon to accounts

--- src/components/AccountView.tsx
+++ src/components/AccountView.tsx
@@ -1,15 +1,21 @@
-import { Accordion, AccordionDetails, AccordionSummary, Typography } from "@mui/material";
+import { Accordion, AccordionDetails, AccordionSummary, Box, Typography } from "@mui/material";
 import { Account } from "../types/Account";
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 interface AccountProps {
     account: Account
 }
 
 export default function AccountView({account}: AccountProps) {
+    // Could add logic here that, based on the currency given by the account, changes the monetary symbol (e.g. kr/$/€/£...)
+
     return (
         <Accordion>
-            <AccordionSummary>
-                <Typography>{account.name}</Typography>
+            <AccordionSummary expandIcon={<ExpandMoreIcon />} sx={{flexDirection: "row-reverse"}}>
+                <Box display="flex" justifyContent="space-between" width="100%">
+                    <Typography ml={1}>{account.name}</Typography>
+                    <Typography fontWeight="bold">{account.balance.toLocaleString("da-DK")} kr</Typography>
+                </Box>
             </AccordionSummary>
             <AccordionDetails>
                 <Typography>Account Id: {account.id}</Typography>
