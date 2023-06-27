93f60dd02589387155137784d8c7dfde7ea0819a
Smilla Dion <smilla.dion@live.dk>
2023-06-27 11:10:59 +0200

    Add text to home page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,7 +1,16 @@
-import { Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 
 export default function Home() {
     return (
-        <Typography>Home</Typography>
+        <Box display="flex" justifyContent="center" flexDirection="column" alignItems="center" mt={15}>
+            <Typography color="#003f62" fontWeight="bold" fontSize={30} mb={2}>
+                Welcome to the Account Management system
+            </Typography>
+            <Typography width={800} fontSize={18} textAlign="center">
+                This website has been created as a practice project based on the code test given by Danske Bank.
+                It is intended as an account management system where a user can see all their accounts and transactions.
+                Select either "Accounts" or "Transactions" in the navigation menu to see it in action.
+            </Typography>
+        </Box>
     )
 }
